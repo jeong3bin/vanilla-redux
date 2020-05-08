ea5d89bb9ee08f3a6065d371d6f6363be5a431e8
jeong3bin <jn8121@naver.com>
2020-05-08 14:49:19 +0900

    react Toolkit(done)

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Vanilla Redux
+
+1. createAction -> 자동적으로 Action 생성
+2. createReducer -> switch X, 새로운 상태 retrun 또는 기존 상태 mutate(redux toolkit 뒤에 immer.js가 뒤에서 작동중이기 때문에 가능)
+3. configureStore
+4. createSlice -> 캡슐화, 상태 mutate 가능
--- src/store.js
+++ src/store.js
@@ -1,41 +1,20 @@
-import { createStore } from "redux";
-import { createReducer, createAction,configureStore } from "@reduxjs/toolkit"
+import { createSlice, configureStore } from "@reduxjs/toolkit"
 
-const addToDo = createAction("ADD");
-const deleteToDo = createAction("DELETE");
 
-// const reducer = (state = [], action) => {
-//     switch (action.type) {
-//         case addToDo.type:
-//             console.log(action);
-//             return [{ text: action.payload, id: Date.now() }, ...state];
-//             // action에게 보내고 싶은 정보가 무엇이든지 payload와 함께 보내짐(toolkit이 제공)
-//         case deleteToDo.type:
-//             return state.filter(toDo => toDo.id !== action.payload);
-//         default:
-//             return state;
+const toDos = createSlice({
+    name: 'toDosRecucer',
+    initialState: [],
+    reducers: {
+        add: (state, action) => {
+            state.push({ text: action.payload, id: Date.now() });
+        },
+        remove: (state, action) =>
+            state.filter(toDo => toDo.id !== action.payload)
+    }
 
-//     }
-// };
+});
 
-//switch 필요 X
-//상태 mutate가능
-//새로운 상태일 때만 return 
-const reducer = createReducer([], {
-    [addToDo]: (state, action) => {
-        state.push({ text: action.payload, id: Date.now() })
-    },
-    [deleteToDo]: (state, action) =>
-        state.filter(toDo => toDo.id !== action.payload)
-})
+export const {add, remove} = toDos.actions;
 
-const store = configureStore({reducer});
-
-export const actionCreators = {
-    addToDo,
-    deleteToDo
-};
-
-export default store;
-
+export default configureStore({ reducer: toDos.reducer });;
 
