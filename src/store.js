6f99fc9b86522c1671c37f5b936998b5f2175003
jeong3bin <jn8121@naver.com>
2020-05-08 11:57:20 +0900

    react Toolkit(createReducer)

--- src/store.js
+++ src/store.js
@@ -1,27 +1,37 @@
 import { createStore } from "redux";
-import {createAction} from "@reduxjs/toolkit"
+import { createReducer, createAction,configureStore } from "@reduxjs/toolkit"
 
 const addToDo = createAction("ADD");
 const deleteToDo = createAction("DELETE");
 
+// const reducer = (state = [], action) => {
+//     switch (action.type) {
+//         case addToDo.type:
+//             console.log(action);
+//             return [{ text: action.payload, id: Date.now() }, ...state];
+//             // action에게 보내고 싶은 정보가 무엇이든지 payload와 함께 보내짐(toolkit이 제공)
+//         case deleteToDo.type:
+//             return state.filter(toDo => toDo.id !== action.payload);
+//         default:
+//             return state;
 
-const reducer = (state = [], action) => {
-    switch (action.type) {
-        case addToDo.type:
-            console.log(action);
-            return [{ text: action.payload, id: Date.now() }, ...state];
-            // action에게 보내고 싶은 정보가 무엇이든지 payload와 함께 보내짐(toolkit이 제공)
-        case deleteToDo.type:
-            return state.filter(toDo => toDo.id !== action.payload);
-        default:
-            return state;
+//     }
+// };
 
-    }
-};
+//switch 필요 X
+//상태 mutate가능
+//새로운 상태일 때만 return 
+const reducer = createReducer([], {
+    [addToDo]: (state, action) => {
+        state.push({ text: action.payload, id: Date.now() })
+    },
+    [deleteToDo]: (state, action) =>
+        state.filter(toDo => toDo.id !== action.payload)
+})
 
-const store = createStore(reducer);
+const store = configureStore({reducer});
 
-export const actionCreators={
+export const actionCreators = {
     addToDo,
     deleteToDo
 };
